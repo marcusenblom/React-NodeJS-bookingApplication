ec7872af2724a98890f36592ef6decf33524ac3f
Marcus Enblom <marcusenblom@gmail.com>
2020-08-24 19:31:35 +0200

    get available tables router

--- api/router/bookingRouter.js
+++ api/router/bookingRouter.js
@@ -1,22 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require("mongoose");
-const {
-    Booking,
-    validateBooking
-} = require('../model/bookingModel');
-const {
-    User,
-    validateUser
-} = require('../model/userModel');
+const { Booking, validateBooking } = require('../model/bookingModel');
+const { User, validateUser } = require('../model/userModel');
+const { Restaurant, validateRestaurant } = require('../model/restaurantModel');
 
-router.get("/getAvailability", async (req, res) => {
+// Importerade Moment i både api samt client för att lättare kunna arbeta med datumformatering
+const moment = require('moment');
+
+router.get("/getAvailability/:date", async (req, res) => {
     
-    // Skickar med user input: Date + # of people
+    // Skickar med user input: Date + # of people. OBS: Just nu så blir datumet en dag tidigare
 
-    res.send(bookings)
+    // Inte klart ännu!!
+    var date = new moment(req.params.date).format('L');
+    const bookings = await Booking.find({date: date});
+    const restaurant = await Restaurant.find({restaurantId: 1});
+    let tableSize = 6;
+
+    let tablesOccupied = 0;
+
+    console.log("Table size: " + tableSize);
+    
+    bookings.forEach(booking => {
+        if (booking.numberOfPeople > tableSize) {
+            tablesOccupied += booking.numberOfPeople % tableSize;
+            console.log(booking.numberOfPeople % tableSize);
+        } else {
+            tablesOccupied += 1;
+        }
+    });
 
     // Få tillbaka: Tillgängliga tider för det datumet / alternativt felmeddelande som säger att det inte finns tillräckligt många bord för det sällskapet
+    if (bookings.length > 0) {
+        res.send("Tables occupied" + tablesOccupied + bookings)
+    } else {
+        res.send(restaurant)
+    }
+    
 
 });
 
@@ -24,11 +45,13 @@ router.post("/createBooking", async (req, res) => {
 
     // Ta in user input för: Förnamn, Efternamn, Mail, Tele. Kolla sedan om mailen är ledig och skapa upp ett user objekt och skickar till databasen. Om mailen redan finns på ett user objekt i databasen så överskrivs övriga inputs (namn, tele) på det redan befintliga user objektet i databasen
 
+    const bookings = await Booking.find();
+
     const booking = new Booking({
-        bookingId: 1,
-        date: new Date(),
+        bookingId: bookings.length + 1,
+        date: new moment("20201111").format('L'),
         time: 18,
-        numberOfPeople: 3,
+        numberOfPeople: 12,
         customerId: 1,
         restaurantId: 1
     });
@@ -39,19 +62,19 @@ router.post("/createBooking", async (req, res) => {
         }
     });
 
-    const user = new User({
-        userId: 1,
-        firstName: "Linda",
-        surName: "Andersson",
-        email: "Hej@gmial.ciom",
-        phoneNumber: 07012312323
-    });
+    // const user = new User({
+    //     userId: 1,
+    //     firstName: "Linda",
+    //     surName: "Andersson",
+    //     email: "Hej@gmial.ciom",
+    //     phoneNumber: 07012312323
+    // });
 
-    await user.save((error, succes) => {
-        if (error) {
-            res.send(error.message)
-        }
-    });
+    // await user.save((error, succes) => {
+    //     if (error) {
+    //         res.send(error.message)
+    //     }
+    // });
 
     res.send("Tillagd");
 
