47d48b2b1633ff285a439b2f36ea5db3a7aee9f4
Marcus Enblom <marcusenblom@gmail.com>
2020-08-27 16:18:06 +0200

    complete user object is now logged on the booking

--- api/model/bookingModel.js
+++ api/model/bookingModel.js
@@ -22,9 +22,34 @@ const bookingSchema = new Schema({
         minlength: 1
     },
     customer: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
+        userId: {
+            type: Number,
+            unique: true,
+            required: true
+        },
+        firstName: {
+            type: String,
+            required: true,
+            minlength: 2,
+            maxlength: 100
+        },
+        lastName: {
+            type: String,
+            required: true,
+            minlength: 2,
+            maxlength: 100
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            maxlength: 100
+        },
+        phoneNumber: {
+            type: Number,
+            required: true,
+            maxlength: 100
+        }
     },
     restaurantId: {
         type: Number,
