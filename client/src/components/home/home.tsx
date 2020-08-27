2efc36de9e9e5c8cfbb5ff1a0f3c302f9b412202
Linda Andersson <lindaandersson@Lindas-MacBook-Pro.local>
2020-08-27 15:20:08 +0200

    scss and app.tsx

--- client/src/components/home/home.tsx
+++ client/src/components/home/home.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import './home.scss';
+import React from "react";
+import "./home.scss";
+import startimg from "../../image/unpoco1.jpg";
 
 export default function Home() {
-    
-    return (
-        <h2>Home Works!</h2>
-    );
+  return (
+    <div className="container">
+      <img className="welcome-img" src={startimg} alt="pizza" />
+
+      <div className="booking-table">
+        <a href="/booking">
+          <button className="booking-table-btn">Reserve a table</button>
+        </a>
+      </div>
+    </div>
+  );
 }
