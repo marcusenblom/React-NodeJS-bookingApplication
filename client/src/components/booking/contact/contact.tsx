564aacf345d2a22d42f41b3ca3c5d3517a300f4f
[Franco] <[francisco.a.jimenez88@gmail.com]>
2020-09-01 11:51:11 +0200

    new push

--- client/src/components/booking/contact/contact.tsx
+++ client/src/components/booking/contact/contact.tsx
@@ -19,10 +19,10 @@ interface IContactProps {
 export default function ContactComponent(props: IContactProps) {
 
 
-  const [firstName, setFirstName] = useState();
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState(0);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState();
 
   const [date, setDate] = useState(new Date());
   const [people, setPeople] = useState(0);
