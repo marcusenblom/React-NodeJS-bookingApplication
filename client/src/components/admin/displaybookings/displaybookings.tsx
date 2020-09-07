b6664e454ddde062e02fbcfa9c0a47723806b401
Marcus Enblom <marcusenblom@gmail.com>
2020-09-07 10:21:34 +0200

    removed error in admin.tsx by creating a handleChange function which is called on DatePicker onChange

--- client/src/components/admin/displaybookings/displaybookings.tsx
+++ client/src/components/admin/displaybookings/displaybookings.tsx
@@ -2,17 +2,36 @@ import React from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import BookingClass from '../../../models/bookingModel';
 import "./../../../scss/_displaybookings.scss";
+import moment from 'moment';
 
 
 interface IDisplayBookingsProps {
     bookings: BookingClass[];
+    // date: Date;
 }
 
 export default function DisplayBookings(props: IDisplayBookingsProps) {
 
+    // let shortDate = moment(date).format('LL');
+
     let bookingLi = props.bookings.map(b => {
+
         return (<div className="booking" key={b.bookingId}>
+            <div className="booking-id">
             Booking ID: {b.bookingId}
+            </div>
+            <div className="customer-id">
+            User ID: {b.customerId}
+            </div>
+            <div className="date">
+            Date: {b.date}
+            </div>
+            <div className="time">
+            Time: {b.time}.00
+            </div>
+            <div className="people">
+            People: {b.numberOfPeople}
+            </div>
             </div>)
     })
 
