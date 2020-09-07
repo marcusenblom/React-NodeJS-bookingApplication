ad255a88f2031bb7e279cbb8a31d7151dcacfd0d
Marcus Enblom <marcusenblom@gmail.com>
2020-09-07 09:37:36 +0200

    added scss file for displaybooking

--- client/src/components/admin/displaybookings/displaybookings.tsx
+++ client/src/components/admin/displaybookings/displaybookings.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import BookingClass from '../../../models/bookingModel';
+import "./../../../scss/_displaybookings.scss";
 
 
 interface IDisplayBookingsProps {
@@ -10,14 +11,14 @@ interface IDisplayBookingsProps {
 export default function DisplayBookings(props: IDisplayBookingsProps) {
 
     let bookingLi = props.bookings.map(b => {
-        return (<div key={b.bookingId}>Booking ID: {b.bookingId}</div>)
+        return (<div className="booking" key={b.bookingId}>
+            Booking ID: {b.bookingId}
+            </div>)
     })
 
     return (
-        <div>
-            <div>
-                {bookingLi}
-            </div>
+        <div className="booking-wrapper">
+            {bookingLi}
         </div>
     );
 }
