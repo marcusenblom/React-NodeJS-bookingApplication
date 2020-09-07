2a43342dd89ab5430629d10115daee3685695f41
[Franco] <[francisco.a.jimenez88@gmail.com]>
2020-09-07 11:11:27 +0200

    fixed some positioning

--- client/src/components/booking/contact/contact.tsx
+++ client/src/components/booking/contact/contact.tsx
@@ -190,7 +190,7 @@ export default function ContactComponent(this: any, props: IContactProps) {
             <label htmlFor="checkbox">I accept gdpr terms</label>
           </div>
 
-          <button onClick={handlePopup} disabled={!formState.isValid}>Reserve a table!</button>
+          <button onClick={handlePopup} disabled={!formState.isValid} className='mb'>Reserve a table!</button>
         </form>
 
         {showPopup && (
