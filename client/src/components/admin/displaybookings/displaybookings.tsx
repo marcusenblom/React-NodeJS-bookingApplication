b67edac2fd3b4cb1f72d9c9d6496d68cff3f2c93
Linda Andersson <lindaandersson@Lindas-MacBook-Pro.local>
2020-09-08 11:27:03 +0200

    yes

--- client/src/components/admin/displaybookings/displaybookings.tsx
+++ client/src/components/admin/displaybookings/displaybookings.tsx
@@ -1,72 +1,78 @@
-import React from 'react';
-import 'react-datepicker/dist/react-datepicker.css';
-import BookingClass from '../../../models/bookingModel';
+import React, { useState } from "react";
+import "react-datepicker/dist/react-datepicker.css";
+import BookingClass from "../../../models/bookingModel";
 import "./../../../scss/_displaybookings.scss";
-import moment from 'moment';
-import Axios from 'axios';
-
+import moment from "moment";
+import Axios from "axios";
 
 interface IDisplayBookingsProps {
-    bookings: BookingClass[];
-    date: Date;
+  bookings: BookingClass[];
+  date: Date;
 }
 
 export default function DisplayBookings(props: IDisplayBookingsProps) {
+  const [bookings, setBookings] = useState([]);
 
-    // Konvertera datumet på bokningsobjekten till en sträng innehållande ett moment-datum
-    let shortDate = moment(props.date).format('LL');
+  // Konvertera datumet på bokningsobjekten till en sträng innehållande ett moment-datum
+  let shortDate = moment(props.date).format("LL");
 
-    function removeBooking(bookingId: number) {
-        Axios.delete('http://localhost:4000/deleteBooking/'+ bookingId).then((res)=>{
-            updateAdmin();
-        })
-    }
-    function updateAdmin(){
-        // axios.get('http://localhost:4000').then((res)=>{
-            
-        // })
-    }
-
-    
-    let bookingLi = props.bookings.map(b => {
-
-        return (<div className="booking" key={b.bookingId}>
-            <div className="id-wrapper booking-wrapper">
-                <div className="booking-id">
-                Booking ID: <strong>{b.bookingId}</strong>
-                </div>
-                <div className="customer-id">
-                User ID: <strong>{b.customerId}</strong>
-                </div>
-            </div>
-
-            <div className="time-people-wrapper booking-wrapper">
-                <div className="time">
-                Time: <strong>{b.time}.00</strong>
-                </div>
-                <div className="people">
-                People: <strong>{b.numberOfPeople}</strong>
-                </div>
-            </div>
-
-            <div className="button-wrapper booking-wrapper">
-                <button>Change</button>
-                <button type="button" onClick={()=>removeBooking(b.bookingId)}>Remove</button>
-            </div>
-        </div>)
-    })
-
-    let header = "";
-    if (props.bookings.length >= 1) {
-        header = `Showing bookings for date: ${shortDate}`
-    } else {
-        header = "No bookings can be found for that specific date"
-    }
-
-    return (
-        <div className="all-bookings">
-            <div className="date-header"><h3>{header}</h3></div>
-            {bookingLi}
-        </div>
+  function removeBooking(bookingId: number) {
+    Axios.delete("http://localhost:4000/deleteBooking/" + bookingId).then(
+      res => {
+        updateAdmin();
+      }
     );
+  }
+  function updateAdmin() {
+    Axios.get("http://localhost:4000").then(res => {
+      setBookings(res.data);
+    });
+  }
+
+  let bookingLi = props.bookings.map(b => {
+    return (
+      <div className="booking" key={b.bookingId}>
+        <div className="id-wrapper booking-wrapper">
+          <div className="booking-id">
+            Booking ID: <strong>{b.bookingId}</strong>
+          </div>
+          <div className="customer-id">
+            User ID: <strong>{b.customerId}</strong>
+          </div>
+        </div>
+
+        <div className="time-people-wrapper booking-wrapper">
+          <div className="time">
+            Time: <strong>{b.time}.00</strong>
+          </div>
+          <div className="people">
+            People: <strong>{b.numberOfPeople}</strong>
+          </div>
+        </div>
+
+        <div className="button-wrapper booking-wrapper">
+          <button>Change</button>
+          <button type="button" onClick={() => removeBooking(b.bookingId)}>
+            Remove
+          </button>
+        </div>
+      </div>
+    );
+  });
+
+  let header = "";
+  if (props.bookings.length >= 1) {
+    header = `Showing bookings for date: ${shortDate}`;
+  } else {
+    header = "No bookings can be found for that specific date";
+  }
+
+  return (
+    <div className="all-bookings">
+      <div className="date-header">
+        <h3>{header}</h3>
+      </div>
+      {bookingLi}
+    </div>
+  );
 }
