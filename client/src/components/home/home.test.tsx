90eabadd3dd58ef4058aa6649f7e7716489d33d1
[Franco] <[francisco.a.jimenez88@gmail.com]>
2020-09-08 10:12:29 +0200

    testing

--- client/src/components/home/home.test.tsx
+++ client/src/components/home/home.test.tsx
@@ -1,1 +1,8 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import Home from './home';
+
+test('the link element "a" should contain "href=/booking".', () => {
+    const { getByText } = render(<Home />)
+    expect(getByText('Reserve a table').closest('a')).toHaveAttribute('href', '/booking');
+});
--- client/src/components/nav/nav.test.tsx
+++ client/src/components/nav/nav.test.tsx
@@ -1,1 +1,9 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import Nav from './nav';
+
+test('should contain the text "FML Restaurant" in a h1 element', () => {
+    const { getByText } = render(<Nav />);
+    const letH1Element = getByText('FML Restaurant');
+    expect(letH1Element).toBeInTheDocument();
+});
