52dc784e2e4be567a479ace48136d5d802234743
[Franco] <[francisco.a.jimenez88@gmail.com]>
2020-09-09 12:50:39 +0200

    update

--- client/src/components/booking/time/time.test.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import TimeComponent from './time';
-
-test('should render contact component on click event', () => {
-});
